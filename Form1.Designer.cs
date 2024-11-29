ee51447c02d42d9109b9b74ab31476f3e87c2600
fahadzakarina <162098919+fahadzakarina@users.noreply.github.com>
2024-11-29 15:37:21 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace PBO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            pictureBox4 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            pictureBox10 = new PictureBox();
+            panel1 = new Panel();
+            label12 = new Label();
+            button5 = new Button();
+            pictureBox3 = new PictureBox();
+            label11 = new Label();
+            label10 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(427, 67);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(199, 225);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(737, 67);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(196, 225);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(437, 295);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 23);
+            label1.TabIndex = 2;
+            label1.Text = "TRAKTOR MERAH";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(1054, 385);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(183, 231);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.tarktor_biru;
+            pictureBox6.Location = new Point(1054, 67);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(183, 225);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(737, 385);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(196, 231);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(754, 295);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 23);
+            label2.TabIndex = 9;
+            label2.Text = "TRAKTOR HIJAU";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(1078, 295);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 23);
+            label3.TabIndex = 10;
+            label3.Text = "TRAKTOR BIRU";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(739, 619);
+            label4.Name = "label4";
+            label4.Size = new Size(194, 23);
+            label4.TabIndex = 11;
+            label4.Text = "ALAT PENGGILING PADI";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(1054, 619);
+            label8.Name = "label8";
+            label8.Size = new Size(181, 23);
+            label8.TabIndex = 15;
+            label8.Text = "ALAT PERONTOK PADI";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(437, 619);
+            label9.Name = "label9";
+            label9.Size = new Size(153, 23);
+            label9.TabIndex = 18;
+            label9.Text = "ALAT TANAM PADI";
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = (Image)resources.GetObject("pictureBox10.Image");
+            pictureBox10.Location = new Point(427, 385);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(199, 231);
+            pictureBox10.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox10.TabIndex = 16;
+            pictureBox10.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(77, 113, 17);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(2, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(267, 1036);
+            panel1.TabIndex = 19;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.FromArgb(195, 233, 86);
+            label12.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = SystemColors.ActiveCaptionText;
+            label12.Location = new Point(73, 659);
+            label12.Name = "label12";
+            label12.Size = new Size(75, 24);
+            label12.TabIndex = 13;
+            label12.Text = "Logout";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(195, 233, 86);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(11, 647);
+            button5.Name = "button5";
+            button5.Size = new Size(222, 53);
+            button5.TabIndex = 12;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(11, 38);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 57);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = SystemColors.ControlLightLight;
+            label11.Location = new Point(152, 52);
+            label11.Name = "label11";
+            label11.Size = new Size(59, 24);
+            label11.TabIndex = 10;
+            label11.Text = "Akun";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.FromArgb(195, 233, 86);
+            label10.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = SystemColors.ActiveCaptionText;
+            label10.Location = new Point(73, 400);
+            label10.Name = "label10";
+            label10.Size = new Size(79, 24);
+            label10.TabIndex = 7;
+            label10.Text = "Cashier";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.FromArgb(195, 233, 86);
+            label7.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ActiveCaptionText;
+            label7.Location = new Point(73, 219);
+            label7.Name = "label7";
+            label7.Size = new Size(100, 24);
+            label7.TabIndex = 6;
+            label7.Text = "Sewa Alat";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(195, 233, 86);
+            label6.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ActiveCaptionText;
+            label6.Location = new Point(73, 127);
+            label6.Name = "label6";
+            label6.Size = new Size(110, 24);
+            label6.TabIndex = 5;
+            label6.Text = "Profit Laba";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(195, 233, 86);
+            label5.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ActiveCaptionText;
+            label5.Location = new Point(73, 310);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Produk";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(195, 233, 86);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(11, 385);
+            button4.Name = "button4";
+            button4.Size = new Size(222, 59);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(195, 233, 86);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(11, 295);
+            button3.Name = "button3";
+            button3.Size = new Size(222, 59);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(195, 233, 86);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(11, 204);
+            button2.Name = "button2";
+            button2.Size = new Size(222, 59);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(195, 233, 86);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(11, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 59);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.White;
+            ClientSize = new Size(1370, 749);
+            Controls.Add(panel1);
+            Controls.Add(label9);
+            Controls.Add(pictureBox10);
+            Controls.Add(label8);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox4);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox8;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label8;
+        private Label label9;
+        private PictureBox pictureBox10;
+        private Panel panel1;
+        private Label label5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label10;
+        private Label label7;
+        private Label label6;
+        private Label label11;
+        private PictureBox pictureBox3;
+        private Label label12;
+        private Button button5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+namespace PBO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
